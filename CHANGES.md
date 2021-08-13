c8e76a2ab64cc184f92898a7af08bda500884cad
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-13 03:11:32 +0000

    start main section

--- CHANGES.md
+++ CHANGES.md
@@ -2,3 +2,6 @@
 - load default font "<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />"
 <!-- Equivalent of normalize.css - kickstart a consistent baseline -->
 - add CSSBaseline
+<!-- Typography API -->
+- [MUI Typography](https://material-ui.com/api/typography/)
+- e.g.: variant, align, color, gutterBottom (true ? then txt has bottom margin)
--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,15 @@ const App = () => {
                     <Typography variant="h6">Photo Album</Typography>
                 </Toolbar>
             </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Photo Album
+                        </Typography>
+                    </Container>
+                </div>
+            </main>
         </>
     )
 }
