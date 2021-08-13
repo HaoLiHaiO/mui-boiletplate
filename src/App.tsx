b1f1dda9bb696a5cb3140c587e84937d27866771
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-13 03:01:23 +0000

    add MUI navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { Typography } from '@material-ui/core'
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
+import { PhotoCamera } from "@material-ui/icons";
 
 const App = () => {
     return (
-        <div>
-            <Typography variant="h1">Hello World</Typography>
-        </div>
+        <>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <PhotoCamera />
+                    <Typography variant="h6">Photo Album</Typography>
+                </Toolbar>
+            </AppBar>
+        </>
     )
 }
 
